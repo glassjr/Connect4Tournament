52599eb3058dbd9e2151a4f281f065197fcee75b
JRock <wax818@gmail.com>
2013-06-02 13:29:02 -0500

    Updated TestRunner.java

--- src/DeveloperTools/TestRunner.java
+++ src/DeveloperTools/TestRunner.java
@@ -6,8 +6,7 @@ public class TestRunner
 {
     public static void main(String[] args)
     {
-        System.out.print("\f");
-        Game g = new Game(new HumanPlayer("Jake"),new RandomAI(), 6,7,4,false);
+        Game g = new Game(new HumanPlayer("Black"),new RandomAI(), 6,7,4,false);
         Player p = g.playGame();
         System.out.println(g);
         System.out.println(p);
